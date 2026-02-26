72861def26f9360aa0f8218931d3e44937e99068
Umaprasanna505 <161013467+Umaprasanna505@users.noreply.github.com>
2026-02-26 10:15:52 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+// ✅ CORRECT ROUTES
+app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/chat", require("./routes/chatRoutes"));
+app.use("/api/message", require("./routes/messageRoutes"));
+
+app.listen(5000, () =>
+    console.log("Server running on port 5000")
+);
